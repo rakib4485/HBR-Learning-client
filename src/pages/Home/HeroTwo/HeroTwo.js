29b368395b780fb6d681920eb43c75c1b5274439
rakib4485 <mdrakibulislam4485@gmail.com>
2023-02-12 00:20:14 +0600

    Home section all component create successfully

---
+++ src/pages/Home/HeroTwo/HeroTwo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroTwo = () => {
+    return (
+        <div>
+            <h2>This is hero two section</h2>
+        </div>
+    );
+};
+
+export default HeroTwo;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Categories from '../Categories/Categories';
+import Explore from '../Explore/Explore';
+import Hero from '../Hero/Hero';
+import HeroTwo from '../HeroTwo/HeroTwo';
+import Review from '../Review/Review';
+import Support from '../Support/Support';
+import TrendingCourses from '../TrendingCourses/TrendingCourses';
 
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <Hero></Hero>
+            <Explore></Explore>
+            <Categories></Categories>
+            <HeroTwo></HeroTwo>
+            <TrendingCourses></TrendingCourses>
+            <Support></Support>
+            <Review></Review>
         </div>
     );
 };
